d4db8ba857979c0dadbc3f207ddef40e461ca859
deevarindu <deevarindu@gmail.com>
2023-02-20 10:38:51 +0700

    new addon

---
+++ praktikum1/__manifest__.py
@@ -0,0 +1,25 @@
+{
+  "name"          : "New Addons",
+  "version"       : "1.0",
+  "author"        : "Deeva Rindu Wijarista Putri",
+  "website"       : "https://www.github.com/deevarindu/",
+  "category"      : "New Module",
+  "summary"       : "Ini adalah addons baru",
+  "description"   : """
+    Latihan membuat addons baru di odoo
+  """,
+  "depends"       : [
+    "base",
+  ],
+  "data"          : [
+
+  ],
+  "demo"          : [],
+  "test"          : [],
+  "image"         : [],
+  "qweb"          : [],
+  "css"           : [],
+  "application"   : True,
+  "installable"   : True,
+  "auto_install"  : False,
+}
